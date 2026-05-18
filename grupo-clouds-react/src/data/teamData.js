80795e9676e7f8d8349a50d98ef6c14cc1ee73d0
Edu Moreno <e.m.morenolp@gmail.com>
2026-05-18 20:33:34 -0300

    add data team

---
+++ grupo-clouds-react/src/data/teamData.js
@@ -0,0 +1,248 @@
+export const teamData = [
+  {
+    slug: 'eduardo',
+    firstName: 'Eduardo',
+    lastName: 'Moreno',
+    role: 'Desarrollador Back-End',
+    roleDetail: 'en Grupo Clouds',
+    location: 'La Plata, Argentina',
+    experience: 'Experiencia en el area Back-End',
+    interest: 'Fuerte interes en Inteligencia Artificial',
+    description: 'Soy desarrollador con experiencia en Back-End y enfocado en integrar soluciones con Inteligencia Artificial.',
+    image: 'https://avatars.githubusercontent.com/u/126937215?v=4',
+    techStack: [
+      { id: 'html', name: 'HTML' },
+      { id: 'css', name: 'CSS' },
+      { id: 'javascript', name: 'JavaScript' },
+      { id: 'nodejs', name: 'Node.js' },
+      { id: 'git', name: 'Git / GitHub' },
+      { id: 'sqlite', name: 'SQLite' },
+      { id: 'vite', name: 'Vite' },
+      { id: 'react', name: 'React' },
+      { id: 'mysql', name: 'MySQL' },
+      { id: 'prisma', name: 'Prisma' },
+      { id: 'docker', name: 'Docker' },
+      { id: 'ai', name: 'Inteligencia Artificial' }
+    ],
+    movies: [
+      {
+        id: 'temblores',
+        title: 'Temblores (Tremors, 1990)',
+        year: '1990',
+        badge: 'HD',
+        genre: 'Terror + comedia',
+        desc: 'Una joya del cine de serie B. En Perfeccion, Nevada, unos gusanos gigantes subterraneos llamados Graboids atacan guiandose por las vibraciones del suelo.',
+        poster: 'https://static.wikia.nocookie.net/doblaje/images/1/11/Temblores.png/revision/latest?cb=20171019001311&path-prefix=es',
+        video: 'https://www.youtube.com/embed/liJfZvXdiTE?autoplay=1'
+      },
+      {
+        id: 'idiocracia',
+        title: 'Idiocracia (Idiocracy, 2006)',
+        year: '2006',
+        badge: 'HD',
+        genre: 'Dirigida por Mike Judge',
+        desc: 'Paso de largo en su estreno y termino como pelicula de culto. Un hombre comun despierta 500 anos en el futuro y descubre que se volvio la persona mas inteligente del planeta.',
+        poster: 'https://play-lh.googleusercontent.com/F4zzqbLEgxjbHhCWM-NGfHR_cN1ABRGHTlDZd3iUTs1CjifKhRMjQVCSIwtrwGCKrnBg',
+        video: 'https://www.youtube.com/embed/sP2tUW0HDHA?autoplay=1'
+      },
+      {
+        id: 'bluesbrothers',
+        title: 'Blues Brothers 2000 (1998)',
+        year: '1998',
+        badge: 'HD',
+        genre: 'Musica y comedia',
+        desc: 'Elwood Blues sale de la carcel y vuelve a reunir la banda con nuevos companeros. Destacan B.B. King, Eric Clapton y Aretha Franklin en un final inolvidable.',
+        poster: 'https://m.media-amazon.com/images/M/MV5BMTkxZmMxODMtODBkYy00MTg0LWI2MjctMTQzNmRiMDY0OTZkXkEyXkFqcGc@._V1_.jpg',
+        video: 'https://www.youtube.com/embed/2eEpcETVV3s?autoplay=1'
+      }
+    ],
+    albums: [
+      'https://open.spotify.com/embed/album/3HCCUaRSjHSFOe4fqE0BiP?utm_source=generator&theme=0',
+      'https://open.spotify.com/embed/album/0Ip2GlQPoAIgdkqCO2YkMa?utm_source=generator&theme=0',
+      'https://open.spotify.com/embed/album/6xj8KDSoPvZCquyIHWo0PJ?utm_source=generator&theme=0'
+    ]
+  },
+  {
+    slug: 'leandro',
+    firstName: 'Leandro',
+    lastName: 'Paryszewski',
+    role: 'Desarrollador y Soporte',
+    roleDetail: 'en Grupo Clouds',
+    location: 'Buenos Aires, Argentina',
+    experience: 'Experiencia en servidores',
+    interest: 'Open Source Technology Fan',
+    description: 'Soy un entusiasta de la tecnología experiencia con servidores, electronica, mecánica, lógistica y ...redoble de tambores "Software".',
+    image: 'https://avatars.githubusercontent.com/u/167145451?v=4',
+    techStack: [
+      { id: 'c', name: 'C' },
+      { id: 'cpp', name: 'C++' },
+      { id: 'asm', name: 'ASM' },
+      { id: 'linux', name: 'Linux / GNU' },
+      { id: 'ssh', name: 'SSH' },
+      { id: 'docker', name: 'Docker' },
+      { id: 'kubernetes', name: 'Kubernetes' },
+      { id: 'bash', name: 'Bash / Shell' },
+      { id: 'embedded', name: 'Embedded / IoT' },
+      { id: 'javascript', name: 'JavaScript' },
+      { id: 'git', name: 'Git / GitHub' },
+      { id: 'webcore', name: 'Web Core' }
+    ],
+    movies: [
+      {
+        id: 'matrix',
+        title: 'Matrix (The Matrix, 1999)',
+        year: '1999',
+        badge: '4K',
+        genre: 'Ciencia Ficción',
+        desc: 'Un clásico de la ciencia ficción que explora la realidad simulada. Ideal para un perfil tecnológico y mentes curiosas.',
+        poster: 'https://static.wikia.nocookie.net/thebigbangtheory/images/7/7a/Matrix.jpg/revision/latest?cb=20100301173222&path-prefix=es',
+        video: 'https://www.youtube.com/embed/nUEQNVV3Gfs?autoplay=1'
+      },
+      {
+        id: 'avatar',
+        title: 'Avatar (2009)',
+        year: '2009',
+        badge: 'HD',
+        genre: 'Aventura + Sci-Fi',
+        desc: 'Una revolución visual y tecnológica en el cine sobre la conexión profunda entre la naturaleza y la tecnología.',
+        poster: 'https://i0.wp.com/www.serendypia.com/wp-content/uploads/2022/12/avatar_es.jpg?fit=1140%2C660&ssl=1',
+        video: 'https://www.youtube.com/embed/5PSNL1qE6VY?autoplay=1'
+      },
+      {
+        id: 'ladrondebicicletas',
+        title: 'Ladrón de bicicletas (1948)',
+        year: '1948',
+        badge: 'B&W',
+        genre: 'Neorrealismo',
+        desc: 'Una obra maestra del cine italiano. Una historia humana profunda sobre la lucha, la perseverancia y la realidad social.',
+        poster: 'https://www.infobae.com/resizer/v2/2NDD4HLK5RDDXIIPPMD7ZSTCUQ.jpg?auth=3d542ee6190ff552b4c7014b82cd0fbc1a971ea8aa7d3ce4c215f22dd03fd402&smart=true&width=992&height=558&quality=85',
+        video: 'https://www.youtube.com/embed/H2P4xo9kmPM?autoplay=1'
+      }
+    ],
+    albums: [
+      'https://open.spotify.com/embed/album/5EyIDBAqhnlkAHqvPRwdbX?utm_source=generator&theme=0',
+      'https://open.spotify.com/embed/album/4eZKmaAjQLChmpJ7uqoZAY?utm_source=generator&theme=0',
+      'https://open.spotify.com/embed/album/2lORE9lVX7YUCZfiFVnKWa?utm_source=generator&theme=0'
+    ]
+  },
+  {
+    slug: 'melisa',
+    firstName: 'Melissa',
+    lastName: 'Galeano',
+    role: 'Desarrolladora y tester',
+    roleDetail: 'en Grupo Clouds',
+    location: 'Buenos Aires, Argentina',
+    experience: '33 años',
+    interest: 'Interés en aseguramiento de calidad de Software',
+    description: 'Apasionada por el análisis y validación de sistemas, con especial interés en testing y mejora continua.',
+    image: 'https://avatars.githubusercontent.com/u/95649292?v=4',
+    techStack: [
+      { id: 'html', name: 'HTML' },
+      { id: 'css', name: 'CSS' },
+      { id: 'git', name: 'Git / GitHub' },
+      { id: 'javascript', name: 'JavaScript' },
+      { id: 'csharp', name: 'C#' },
+      { id: 'kotlin', name: 'Kotlin' },
+      { id: 'mysql', name: 'MySQL' }
+    ],
+    movies: [
+      {
+        id: 'ventanasecreta',
+        title: 'La venta secreta',
+        year: '2004',
+        badge: 'HD',
+        genre: 'Terror/Misterio',
+        desc: 'Un escritor atraviesa uno de los peores momentos de su vida: se acaba de divorciar y le falla la creatividad, por ello se refugia en las montañas, donde aparece un extraño que lo acusa de plagio. Entonces, empiezan a pasar cosas inexplicables.',
+        poster: 'https://m.media-amazon.com/images/M/MV5BNTNlZDFiOTItMTAyNC00MGE2LWE1ZTUtM2M3ZmZmZDY2ZjdiXkEyXkFqcGc@._V1_QL75_UX582_.jpg',
+        video: 'https://www.youtube.com/embed/P1t2xgtAgCQ?autoplay=1'
+      },
+      {
+        id: 'losotros',
+        title: 'Los otros',
+        year: '2001',
+        badge: 'HD',
+        genre: 'Terror/Misterio',
+        desc: 'Una viuda afligida descubre sucesos extraños, que apuntan hacia la presencia de fantasmas en su apartada mansión.',
+        poster: 'https://m.media-amazon.com/images/M/MV5BMTMyNzYwMTExNF5BMl5BanBnXkFtZTcwOTg1MzU5Ng@@._V1_QL75_UX489_.jpg',
+        video: 'https://www.youtube.com/embed/f9Hoj951nSA?autoplay=1'
+      },
+      {
+        id: 'asienlatierra',
+        title: 'Así en la Tierra como en el Infierno',
+        year: '2014',
+        badge: 'B&W',
+        genre: 'Terror/Aventura',
+        desc: 'Kilómetros y kilómetros de tortuosas catacumbas yacen bajo las calles de París, plagadas de incontables almas. Cuando un equipo de exploradores se aventura en este desconocido laberinto, se descubre cuál fue la verdadera función de esta ciudad.',
+        poster: 'https://cdn.cinencuentro.com/wp-content/uploads/2014/11/Asi-en-la-tierra-como-en-el-infierno-As-Above-So-Below.jpg',
+        video: 'https://www.youtube.com/embed/aVkKt33be6Y?autoplay=1'
+      }
+    ],
+    albums: [
+      'https://open.spotify.com/embed/album/2knEuvsxqHMAoxlQpIdpQD?utm_source=generator&theme=0',
+      'https://open.spotify.com/embed/album/0lldWAcuS8LXxoXJuU4Yca?utm_source=generator&theme=0',
+      'https://open.spotify.com/embed/album/7DVYoAnZl4Jg7uaglJyWkP?utm_source=generator&theme=0'
+    ]
+  },
+  {
+    slug: 'marcelo',
+    firstName: 'Marcelo',
+    lastName: 'Moreno',
+    role: 'Product Designer',
+    roleDetail: 'en Grupo Clouds',
+    location: 'Buenos Aires, Argentina',
+    experience: '30 años',
+    interest: 'Freelancer',
+    description: 'Soy un desarollador entusiasta de la Inteligencia Artificial y Atleta de Streetlifting',
+    image: 'https://lh3.googleusercontent.com/aida-public/AB6AXuAKV-JJu4hJv6q7cuHbqADHu2kxOoiVw43rI3oZk8a4omdJd6g2upNAu2ppLs7MuIjBn0B3qT8sAy_353poG368kLRuyNaKUwtLt7LClxAIFBt0OTw3A7mspCcwxPKIUX-FXiwk91oxAmYgKKEnFcVpyVMFAFtzrqSNL1rpaJxJUd42Bq2dSvM2sMGTGNVgWbGM6s3LPCfw-U4wkW2LD78-3Awy5UCLSu8CgKD16fD3-_Ug0OJLdXzIkS_W80wStOa5soBQpPRPgNdf',
+    techStack: [
+      { id: 'html', name: 'HTML' },
+      { id: 'css', name: 'CSS' },
+      { id: 'git', name: 'Git / GitHub' },
+      { id: 'javascript', name: 'JavaScript' },
+      { id: 'typescript', name: 'TypeScript' },
+      { id: 'csharp', name: 'C#' },
+      { id: 'kotlin', name: 'Kotlin' },
+      { id: 'sql', name: 'Bases de datos SQL' }
+    ],
+    movies: [
+      {
+        id: 'esdla',
+        title: 'El Señor de los Anillos: La Comunidad del Anillo',
+        year: '2001',
+        badge: 'HD',
+        genre: 'Dirigida por Peter Jackson',
+        desc: 'La épica aventura de Frodo Bolsón y la Comunidad en su misión para destruir el Anillo Único y salvar la Tierra Media de la oscuridad de Sauron.',
+        poster: 'https://cineciutat.org/storage/app/uploads/public/654/930/261/thumb_3860_1920_1080_0_0_crop.jpg',
+        video: 'https://www.youtube.com/embed/V75dMMIW2B4?autoplay=1',
+        awards: '4 Premios Óscar'
+      },
+      {
+        id: 'gladiator',
+        title: 'Gladiator',
+        year: '2000',
+        badge: 'HD',
+        genre: 'Dirigida por Ridley Scott',
+        desc: 'Un general romano traicionado busca venganza contra el emperador corrupto que asesinó a su familia, luchando como gladiador en el Coliseo de Roma.',
+        poster: 'https://www.studioremarkable.com/wp-content/uploads/2000/05/Gladiator-Still.jpg',
+        video: 'https://www.youtube.com/embed/P5ieIbInFpg?autoplay=1',
+        awards: '5 Premios Óscar'
+      },
+      {
+        id: 'demonslayer',
+        title: 'Demon Slayer: El tren infinito',
+        year: '2020',
+        badge: 'HD',
+        genre: 'Dirigida por Haruo Sotozaki',
+        desc: 'Tanjiro y sus compañeros se embarcan en una misión a bordo del Tren Infinito junto al Pilar de la Llama, Kyōjurō Rengoku, enfrentándose a un poderoso demonio.',
+        poster: 'https://external-preview.redd.it/kimetsu-no-yaiba-the-movie-mugen-train-new-key-visual-v0-JvNsx-vE-zFAdi_cJMpvDiOAFfJG5Sw2lQBQwGvp4wE.jpg?auto=webp&s=dcf83c7810691804b137be9add9712bb3d8bfcf9',
+        video: 'https://www.youtube.com/embed/-AwLMRgcEoA?autoplay=1',
+        awards: 'Película de Anime #1 a nivel global'
+      }
+    ],
+    albums: [
+      'https://open.spotify.com/embed/album/7o4UsmV37Sg5It2Eb7vHzu?utm_source=generator&theme=0',
+      'https://open.spotify.com/embed/album/4J79cxsmRqZbI0BKmNFDy2?utm_source=generator&theme=0',
+      'https://open.spotify.com/embed/album/4Io5vWtmV1rFj4yirKb4y4?utm_source=generator'
+    ]
+  }
+];
